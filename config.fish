ece4b76a4f18b096a19042242d0e7ed3bd5b0fab
zhpinkman <37985462+zhpinkman@users.noreply.github.com>
2020-09-03 14:42:29 +0430

    Update config.fish

--- config.fish
+++ config.fish
@@ -6,11 +6,14 @@ function setenv
         set -gx $argv
     end
  end
+function vpn --wraps='sudo openconnect s11.de.weserv.xyz < ~/.config/vpn_conf' --description 'alias vpn=sudo openconnect s11.de.weserv.xyz < ~/.config/vpn_conf'
+    sudo openconnect s11.de.weserv.xyz <~/.config/vpn_conf $argv
+end
+
 
 
 set -g -x fish_greeting ''
 set -gx PATH $PATH /snap/bin
-set -gx PATH $PATH /home/zhivar/Documents/android/android-studio/bin
-set -gx PATH $PATH /home/zhivar/Android/Sdk/platform-tools
-alias vpn='sudo openconnect s32.de.weserv.xyz < ~/.config/vpn_conf'
-set -gx JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
+# set -gx PATH $PATH /home/zhivar/Documents/android/android-studio/bin
+# set -gx PATH $PATH /home/zhivar/Android/Sdk/platform-tools
+# set -gx JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
